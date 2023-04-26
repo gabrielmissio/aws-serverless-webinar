c764d6f99cfad4d3aae5dd015ebb5e2b8762c3d8
gabriel.missio <gabriel.missio@compasso.com.br>
2023-04-26 16:58:18 -0300

    Validate "users" endpoints with validator middleware and Joi schemas

--- api/src/main/config/routes.js
+++ api/src/main/config/routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const validator = require('../middlewares/validator')
 const userController = require('../../presentation/controllers/user-controller')
+const userSchemas = require('../../presentation/validators/user-schemas')
 
 const router = express.Router()
 
-router.post('/users', userController.createUser)
-router.get('/users/:idOrEmail', userController.getUser)
-router.patch('/users/:idOrEmail', userController.updateUser)
-router.delete('/users/:idOrEmail', userController.deleteUser)
+router.post('/users', validator(userSchemas.createUser), userController.createUser)
+router.get('/users/:idOrEmail', validator(userSchemas.getUser), userController.getUser)
+router.patch('/users/:idOrEmail', validator(userSchemas.updateUser), userController.updateUser)
+router.delete('/users/:idOrEmail', validator(userSchemas.deleteUser), userController.deleteUser)
 
 module.exports = router
---
+++ api/src/presentation/validators/user-schemas.js
@@ -0,0 +1,30 @@
+const Joi = require('joi')
+
+module.exports.createUser = Joi.object({
+  body: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required()
+  })
+}).unknown(true)
+
+module.exports.getUser = Joi.object({
+  params: Joi.object({
+    idOrEmail: Joi.string().required()
+  })
+}).unknown(true)
+
+module.exports.updateUser = Joi.object({
+  params: Joi.object({
+    idOrEmail: Joi.string().required()
+  }),
+  body: Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email()
+  })
+}).unknown(true)
+
+module.exports.deleteUser = Joi.object({
+  params: Joi.object({
+    idOrEmail: Joi.string().required()
+  })
+}).unknown(true)
