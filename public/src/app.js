da56921de4b2844b1fb30d04633da54921b850f3
gabriel.missio <gabriel.missio@compasso.com.br>
2023-04-26 22:05:08 -0300

    Restyle "My User Profile" page

--- public/src/app.js
+++ public/src/app.js
@@ -1,22 +1,37 @@
-const container = document.getElementById('container');
+const container = document.querySelector('.container')
 
 // Make a GET request to the getUser endpoint of your user API
-fetch('http://localhost:3000/dev/user')
+fetch('http://localhost:3000/users/17ed9632-7c45-4217-b7e4-6871df5a70dc')
   .then(response => response.json())
   .then(user => {
     // Create some HTML elements to display the user information
-    const name = document.createElement('h2');
-    name.textContent = user.name;
+    const profile = document.createElement('div')
+    profile.className = 'profile'
 
-    const email = document.createElement('p');
-    email.textContent = user.email;
+    const profileInfo = document.createElement('div')
+    profileInfo.className = 'profile-info'
+
+    const name = document.createElement('h2')
+    name.textContent = user.name
+
+    const email = document.createElement('p')
+    email.textContent = user.email
+
+    const profileImage = document.createElement('img')
+    profileImage.className = 'profile-image'
+    profileImage.src = user.profileUrl || '../media/default-profile-icon.png'
 
     // Add the HTML elements to the page
-    container.innerHTML = '';
-    container.appendChild(name);
-    container.appendChild(email);
+    profileInfo.appendChild(name)
+    profileInfo.appendChild(email)
+
+    profile.appendChild(profileImage)
+    profile.appendChild(profileInfo)
+
+    container.innerHTML = ''
+    container.appendChild(profile)
   })
   .catch(error => {
     // Display an error message if the request fails
-    container.innerHTML = `<p>Error: ${error.message}</p>`;
-  });
+    container.innerHTML = `<p>Error: ${error.message}</p>`
+  })
