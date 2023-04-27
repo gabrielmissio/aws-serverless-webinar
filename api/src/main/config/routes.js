60969d4295cb599abca5fec208bddeb0014b6d4e
gabriel.missio <gabriel.missio@compasso.com.br>
2023-04-27 10:10:22 -0300

    Add get all users endpoint

--- api/src/main/config/routes.js
+++ api/src/main/config/routes.js
@@ -6,6 +6,7 @@ const userSchemas = require('../../presentation/validators/user-schemas')
 const router = express.Router()
 
 router.post('/users', validator(userSchemas.createUser), userController.createUser)
+router.get('/users', validator(userSchemas.getAllUsers), userController.getAllUsers)
 router.get('/users/:idOrEmail', validator(userSchemas.getUser), userController.getUser)
 router.patch('/users/:idOrEmail', validator(userSchemas.updateUser), userController.updateUser)
 router.delete('/users/:idOrEmail', validator(userSchemas.deleteUser), userController.deleteUser)
