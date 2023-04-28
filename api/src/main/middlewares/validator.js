78f22a4507a669c421c512978f48b7d9e9b99f22
gabriel.missio <gabriel.missio@compasso.com.br>
2023-04-28 09:05:31 -0300

    adjust validation middleware to validate only "body", "query" and "params" fields

--- api/src/main/middlewares/validator.js
+++ api/src/main/middlewares/validator.js
@@ -1,7 +1,8 @@
 
 module.exports = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req)
+    const { body, query, params } = req
+    const { error } = schema.validate({ body, query, params })
 
     if (error) {
       return res.status(400).json({ error: error.details[0].message })
